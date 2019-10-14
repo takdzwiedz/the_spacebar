7d01c0d7e0e44b9174ae70e7ff6ded9a3a876ddf
Artur Kacprzak <artur.kacprzak@itcenter.pl>
2019-10-14 14:28:27 +0200

    Updating an Entity and JS

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -60,13 +60,15 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $em)
     {
-        //TODO actually heart/unheart the article
+        $article->incrementHeartCount();
+
+        $em->flush();
 
         $logger->info('Article is being hearted');
 
-        return $this->json(['hearts' => rand(5, 100)]);
+        return $this->json(['hearts' => $article->getHeartCount()]);
 
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -143,4 +143,10 @@ class Article
     {
         return 'images/'.$this->getImageFilename();
     }
+    public function incrementHeartCount():self
+    {
+        $this->heartCount= $this->heartCount + 1;
+
+        return $this;
+    }
 }
